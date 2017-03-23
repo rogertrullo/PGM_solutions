05a07b5cc5c858e810520bcca886d23e1843a53d
rogertrullo <rogertrullo@hotmail.com>
2017-03-23 12:10:50 -0400

    Add files via upload

---
+++ assignment3/ComputeAllSimilarityFactors.m
@@ -0,0 +1,32 @@
+function factors = ComputeAllSimilarityFactors (images, K)
+% This function computes all of the similarity factors for the images in
+% one word.
+%
+% Input:
+%   images: An array of structs containing the 'img' value for each
+%     character in the word.
+%   K: The alphabet size (accessible in imageModel.K for the provided
+%     imageModel).
+%
+% Output:
+%   factors: Every similarity factor in the word. You should use
+%     ComputeSimilarityFactor to compute these.
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+n = length(images);
+nFactors = nchoosek (n, 2);
+
+factors = repmat(struct('var', [], 'card', [], 'val', []), nFactors, 1);
+
+% Your code here:
+b = nchoosek(1:n,2);
+for i=1:length(factors)
+    factors(i) = ComputeSimilarityFactor (images, K, b(i,1), b(i,2));
+    
+end
+
+
+
+end
+
